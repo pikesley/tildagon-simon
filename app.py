c77500e056d58419271784ca59eb0a7f98d9d5ac
pikesley <sam.pikesley@demandlogic.co.uk>
2025-02-09 12:37:51 +0000

    Usable panels

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from time import ticks_ms
+
 from events.input import BUTTON_TYPES, Buttons
 from system.eventbus import eventbus
 from system.patterndisplay.events import PatternDisable
@@ -5,10 +7,8 @@ from tildagonos import tildagonos
 
 import app
 
-from .pikesley.rgb_from_hue.rgb_from_hue import rgb_from_hue
-from .lib.conf import conf
-from .lib.gamma import gamma_corrections
 from .lib.background import Background
+from .lib.conf import conf
 from .lib.panel import Panel
 
 
@@ -20,35 +20,62 @@ class Simon(app.App):
         eventbus.emit(PatternDisable())
         self.button_states = Buttons(self)
 
+        self.panels = [Panel(item) for item in conf["panels"]]
+        self.last_change = ticks_ms()
+
+        self.sequence = [0, 3, 1, 2, 3]
+        # self.sequence = [0]
+        self.sequence_index = 0
+
+        self.game_state = "PLAY"
+
     def update(self, _):
         """Update."""
-        self.scan_buttons()
-
         for i in range(12):
             tildagonos.leds[i + 1] = (0, 0, 0)
+
+        for panel in self.panels:
+            panel.light_leds()
+
+        if self.game_state == "GUESS":
+            for panel in self.panels:
+                panel.deactivate()
+            self.scan_buttons()
+
+        if self.game_state == "PLAY":
+            self.play_sequence()
+
+    def play_sequence(self):
+        """Play the sequence."""
+        self.panels[self.sequence[self.sequence_index]].activate()
+        for panel in self.panels:
+            panel.deactivate()
+
+        if ticks_ms() - self.last_change > conf["panel"]["intervals"]["activatable"]:
+            if self.sequence_index < len(self.sequence) - 1:
+                self.sequence_index += 1
+            else:
+                self.game_state = "GUESS"
+
+            self.last_change = ticks_ms()
 
     def draw(self, ctx):
         """Draw."""
         self.overlays = []
         self.overlays.append(Background(colour=(0, 0, 0)))
 
-        for panel in conf["panels"]:
-            self.overlays.append(Panel(
-                angle=panel["angle"],
-                hue=panel["hue"]
-                ))
-
-            colour = rgb_from_hue(panel["hue"])
-            for index in panel["leds"]:
-                tildagonos.leds[index] = [
-                gamma_corrections[int(c * 255 * conf["leds"]["brightness"]["default"])]
-                for c in colour
-            ]
+        for panel in self.panels:
+            self.overlays.append(panel)
 
         self.draw_overlays(ctx)
 
     def scan_buttons(self):
         """Buttons."""
+        for panel in self.panels:
+            if self.button_states.get(BUTTON_TYPES[panel.button]):
+                self.button_states.clear()
+                panel.activate()
+
         if self.button_states.get(BUTTON_TYPES["CANCEL"]):
             self.button_states.clear()
             self.minimise()
--- lib/panel.py
+++ lib/panel.py
@@ -1,39 +1,91 @@
 from math import radians
+from time import ticks_ms
+from tildagonos import tildagonos
 
 from ..pikesley.rgb_from_hue.rgb_from_hue import rgb_from_hue
 from .conf import conf
+from .gamma import gamma_corrections
 
 
 class Panel:
     """Panel."""
 
-    def __init__(self, angle, hue, opacity=0.7):
+    def __init__(self, data):
         """Construct."""
-        self.angle = angle
-        self.hue = hue
-        self.colour = list(rgb_from_hue(hue)) + [opacity]
+        self.angle = data["angle"]
+        self.hue = data["hue"]
+        self.leds = data["leds"]
+        self.button = data["button"]
+        self.colours = {
+            "plain": list(rgb_from_hue(self.hue)) + [conf["panel"]["opacities"]["plain"]],
+            "border": list(rgb_from_hue(self.hue)) + [conf["panel"]["opacities"]["border"]],
+            "active": list(rgb_from_hue(self.hue)) + [conf["panel"]["opacities"]["active"]],
+        }
+        self.colour = self.colours["plain"]
+        self.semi_arc = conf["panel"]["arc"] / 2
+
+        self.active = False
+        self.activatable = True
+        self.activation_timer = 0
 
     def draw(self, ctx):
         """Draw ourself."""
-        semi_arc = conf["panel"]["arc"] / 2
         ctx.rotate(radians(self.angle))
-        ctx.rgba(*self.colour)
+
+        ctx.rgba(*self.colours["plain"])
+        if self.active:
+            ctx.rgba(*self.colours["active"])
+
+        self.make_shape(ctx)
+        ctx.fill()
+
+        ctx.rgba(*self.colours["border"])
+        self.make_shape(ctx)
+        ctx.stroke()
+
+    def make_shape(self, ctx):
+        """Make the shape."""
         ctx.arc(
             0,
             0,
             conf["panel"]["radius"],
-            radians(semi_arc),
-            radians(-semi_arc),
+            radians(self.semi_arc),
+            radians(-self.semi_arc),
             True,  # noqa: FBT003
         )
         ctx.arc(
             0,
             0,
             conf["panel"]["radius"] - conf["panel"]["depth"],
-            radians(-(semi_arc - 1)),
-            radians(semi_arc - 1),
+            radians(-(self.semi_arc - 0.5)),
+            radians(self.semi_arc - 0.5),
             False,  # noqa: FBT003
         )
         ctx.close_path()
 
-        ctx.fill()
+    def light_leds(self):
+        """Light our LEDs."""
+        colour = rgb_from_hue(self.hue)
+        brightness = conf["leds"]["brightness"]["default"]
+        if self.active:
+            brightness = conf["leds"]["brightness"]["active"]
+
+        for index in self.leds:
+            tildagonos.leds[index] = [
+                gamma_corrections[int(c * 255 * brightness)] for c in colour
+            ]
+
+    def activate(self):
+        """Briefly activate ourself."""
+        if not self.active and self.activatable:
+            self.active = True
+            self.activation_timer = ticks_ms()
+            self.activatable = False
+
+    def deactivate(self):
+        """Should we turn off."""
+        if ticks_ms() - self.activation_timer > conf["panel"]["intervals"]["activation"]:
+            self.active = False
+
+        if ticks_ms() - self.activation_timer > conf["panel"]["intervals"]["activatable"]:
+            self.activatable = True
